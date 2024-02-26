cda1be8dc482078171b8e1166fbb94745be41ab1
ahmadpht <ahmadpirhayati7777@gmail.com>
2024-02-26 15:26:22 +0330

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+export default function footer() {
+  return (
+    <div className='footer'>
+      <div id="footer">
+                <h3><span class="fa-brands fa-linkedin"></span> linkedin</h3>
+                <h3><span class="fa-brands fa-instagram"></span> instagram</h3>
+                <h3><span class="fa-brands fa-telegram"></span> telegram</h3>
+                <h3><span class="fa-brands fa-whatsapp"></span> whatsapp</h3>
+                <h3><span class="fa-brands fa-github"></span> github</h3>
+                <h3><span class="fa-solid fa-phone"></span> number</h3>
+            </div>
+    </div>
+  )
+}
