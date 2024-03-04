405c14ccd45b88c8f3077c3bffacdaa36e9cd645
ahmadpht <ahmadpirhayati7777@gmail.com>
2024-03-04 16:09:35 +0330

    error

--- src/pages/Home/Country/CountryInfo.jsx
+++ src/pages/Home/Country/CountryInfo.jsx
@@ -1,18 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import { useState , useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import './CountryInfo.css'
 
 export const CountryInfo = () => {
 
   const [item, setItem] = useState({})
-
+  const [name , setName] = useState({})
   const params = useParams();
 
   useEffect(() => {
-      axios.get(`https://restcountries.com/v3.1/name/${params.id}`)
-        .then(res => console.log(res))
-        .then(res => setItem(res))
-        .then(console.log(item))
-    },[])
 
+    axios.get(`https://restcountries.com/v3.1/name/${params.id}`)
+      .then(res =>{
+        setItem(res.data[0])
+        setName(res.data[0].name)
+      })    
+      
+    },[])
+      console.log(item);
+      
+    return(
+      <div className='country-info'>
+      <div className='info'>
+        <h1>{name.common}</h1>
+        <h1>{name.official}</h1>
+      </div>
+      </div>
+    )
 }
